96b5f62da0d6f7b866ada65da34f10432afb3454
mohd-sadab-au3 <sadabkhan14198@gmail.com>
2019-09-12 01:11:34 +0530

    mmmm

--- petprofile.js
+++ petprofile.js
@@ -1,6 +1,6 @@
 var express=require("express");
 var router=express.Router();
-
+ 
 router.get("/:id",function(req,res){
     var db=req.app.locals.db ;
     db.collection("petsinfo").find({_id:require("mongodb").ObjectId(req.params.id)}).toArray(function(err,result){
@@ -9,9 +9,9 @@ router.get("/:id",function(req,res){
     res.render("petprofile",{
         petprofile:result[0],
         title:result[0].name,
-        loggedin:req.session.loggedIn,
-        user:req.session.username
+        loggedin:req.session.loggedIn
+
+    });
 });
 });
-});
-module.exports=router;
+module.exports=router
